511fda7ff0714363f137fe6d30ad2b8714554954
kg4mvp <48932663+kg4mvp@users.noreply.github.com>
2020-04-09 21:54:19 -0500

    Create AlertReceived911Activity.kt

---
+++ AlertReceived911Activity.kt
@@ -0,0 +1,11 @@
+package com.example.crisisconnect
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AlertReceived911: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.alert_recieved_911)
+    }
+}
